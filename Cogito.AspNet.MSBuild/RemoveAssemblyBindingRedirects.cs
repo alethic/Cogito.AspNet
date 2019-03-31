d5f48805bb805850fc7a97e8fbf03da6602cd184
Jerome Haltom <wasabi@cogito.cx>
2019-03-31 11:31:51 -0500

    Purge existing binding redirects before build.

---
+++ Cogito.AspNet.MSBuild/RemoveAssemblyBindingRedirects.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+
+using Microsoft.Build.Framework;
+
+namespace Cogito.AspNet.MSBuild
+{
+
+    /// <summary>
+    /// Removes the assembly binding redirects from the given file.
+    /// </summary>
+    public class RemoveAssemblyBindingRedirects : Microsoft.Build.Utilities.Task
+    {
+
+        /// <summary>
+        /// File from which to remove binding redirects.
+        /// </summary>
+        [Required]
+        public ITaskItem File { get; set; }
+
+        public override bool Execute()
+        {
+            var file = XDocument.Load(File.ItemSpec);
+            file.Root.Element("runtime")?.Elements("assemblyBinding")?.Remove();
+            file.Save(File.ItemSpec);
+
+            return true;
+        }
+
+    }
+
+}
